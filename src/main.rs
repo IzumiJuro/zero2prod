e840a1300537dbba46764aacf9f1fe67f766c844
Shen Bing <aseuo0726@gmail.com>
2023-08-01 09:16:52 +0800

    Basic API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::Result, net::TcpListener};
+use sqlx::PgPool;
+use zero2prod::{configuration::get_configuration, startup::run};
+#[tokio::main]
+async fn main() -> Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect Postgres.");
+    let port = configuration.application_port;
+    let address = format!("127.0.0.1:{port}");
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection.clone())?.await
 }
